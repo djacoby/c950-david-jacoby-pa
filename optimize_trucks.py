bdf46fd1ff3b5eaa68da1be5168e3e5af6f85cc5
djacoby <jacoby934@gmail.com>
2021-07-15 21:41:48 -0400

    Fix comments

--- optimize_trucks.py
+++ optimize_trucks.py
@@ -3,7 +3,8 @@ from load_trucks import get_hash_table
 
 hash_table = get_hash_table()
 
-# Read distance & address csv files
+# Read distance & address csv files 
+# Big O = O(1)
 with open('./data/distances.csv', 'r', encoding='utf-8-sig') as distance_csv:
     distance_list = list(csv.reader(distance_csv))
 
@@ -11,19 +12,22 @@ with open('./data/addresses.csv', 'r', encoding='utf-8-sig') as address_csv:
     address_list = list(csv.reader(address_csv))
 
 # Lookup address
+# Big O = O(n)
 def address_lookup(address):
     for entry in address_list:
         if entry[2] == address:
             return int(entry[0])
 
-
+# Get total distance traveled
+# Big O = O(1)
 def get_total_distance(total, curr_index, dest_index):
     distance = distance_list[curr_index][dest_index]
     if distance == '':
         distance = [dest_index][curr_index]
     return total + float(distance)
 
-
+# Get distance from current location to next location
+# Big O = O(1)
 def get_current_distance(curr_index, dest_index):
     distance = distance_list[curr_index][dest_index]
     if distance == '':
@@ -32,16 +36,21 @@ def get_current_distance(curr_index, dest_index):
 
 
 # Truck lists for packages
-first_truck = []
 first_truck_indices = []
 
-second_truck = []
 second_truck_indices = []
 
-third_truck = []
 third_truck_indices = []
 
-
+# Recursive function that caculates the shortest distance to the next delivery point using a "Greedy" Algorithm.
+# The outer loop looks up every package in the hash table then checks if the distances to it is the closest distance
+# from the current distance, if so it sets that is the new lowest_distance
+#
+# The inner loop then checks if the current package distance is equal to the lowest distance, if so it places it on the
+# truck and pops that id from the list then sets the current_location to the location of the package that was placed on 
+#
+# the truck. Then it recursively calls calc_shortest_distance() with the shorten load list, truck number, and current location
+# Big O = O(n^2)
 def calc_shortest_distance(load, truck, curr_location):
     if len(load) == 0:
         return load
@@ -50,6 +59,7 @@ def calc_shortest_distance(load, truck, curr_location):
             lowest_distance = 50.0
             location = 0
 
+            # Big O = O(n)
             for id in load:
                 package = hash_table.lookup(id)
                 next_location = address_lookup(package.address)
@@ -58,28 +68,26 @@ def calc_shortest_distance(load, truck, curr_location):
                     lowest_distance = get_current_distance(
                         curr_location, next_location)
                     location = next_location
-
+            
+            # Big O = O(n)
             for id in load:
                 package = hash_table.lookup(id)
                 next_location = address_lookup(package.address)
 
                 if get_current_distance(curr_location, next_location) == lowest_distance:
                     if truck == 1:
-                        first_truck.append(package)
                         first_truck_indices.append(package.id)
                         load.pop(load.index(id))
                         curr_location = location
                         calc_shortest_distance(load, 1, curr_location)
 
                     elif truck == 2:
-                        second_truck.append(package)
                         second_truck_indices.append(package.id)
                         load.pop(load.index(id))
                         curr_location = location
                         calc_shortest_distance(load, 2, curr_location)
 
                     elif truck == 3:
-                        third_truck.append(package)
                         third_truck_indices.append(package.id)
                         load.pop(load.index(id))
                         curr_location = location
@@ -87,30 +95,22 @@ def calc_shortest_distance(load, truck, curr_location):
         except IndexError:
             pass
 
-
+# Get filled hash table
+# Big O = O(1)
 def get_hash_table():
     return hash_table
 
-
-def get_first_truck():
-    return first_truck
-
-
+# Get optimized first truck package indices
+# Big O = O(1)
 def get_first_truck_indices():
     return first_truck_indices
 
-
-def get_second_truck():
-    return second_truck
-
-
+# Get optimized second truck package indices
+# Big O = O(1)
 def get_second_truck_indices():
     return second_truck_indices
 
-
-def get_third_truck():
-    return third_truck
-
-
+# Get optimized third truck package indices
+# Big O = O(1)
 def get_third_truck_indices():
     return third_truck_indices
--- package.py
+++ package.py
@@ -12,106 +12,106 @@ class Package:
         self.status = 'Not Delivered'
         self.time_delivered = None
 
-    # Getters
+    # Getters (Accessor functions)
     
     # Get package id
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_id(self):
         return self.id
     
     # Get package destination address
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_address(self):
         return self.address
 
     # Get package destination city
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_city(self):
         return self.city
 
     # Get package destination state
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_state(self):
         return self.state
 
     # Get package destination zip
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_zip(self):
         return self.zip
 
     # Get delivery deadline
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_deadline(self):
         return self.deadline
 
     # Get package weight
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_weight(self):
         return self.weight
 
     # Get package special instructions
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_special_instructions(self):
         return self.special_instructions
 
      # Get package delivered time
-     # Big 0 = 0(1)
+     # Big O = O(1)
     def get_time_delivered(self):
         return self.time_delivered
 
     # Get package status (Delivered/ Not delivered)
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def get_status(self):
         return self.status
 
-    # Setters
+    # Setters (Mutator functions)
 
     # Set package id
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_id(self, id):
         self.id = id
 
     # Set package destination address
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_address(self, address):
         self.address = address
 
     # Set package destination city
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_city(self, city):
         self.city = city
 
     # Set package destination state
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_state(self, state):
         self.state = state
 
     # Set package destination zip
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_zip(self, zip):
         self.zip = zip
 
     # Set package delivery deadline
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_deadline(self, deadline):
         self.deadline = deadline
 
     # Set package weight
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_weight(self, weight):
         self.weight = weight
 
     # Set package delivery special instructions
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_special_instructions(self, special_instructions):
         self.special_instructions = special_instructions
 
     # Set package time delivered
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_time_delivered(self, time_delivered):
         self.time_delivered = time_delivered
 
     # Set package status (Delivered, Not delivered)
-    # Big 0 = 0(1)
+    # Big O = O(1)
     def set_status(self, status):
         self.status = status
